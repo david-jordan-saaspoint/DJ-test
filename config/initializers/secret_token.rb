185dcbee3d57757776547b66ed50f602f43dc19a
David Jordan <david.jordan@saaspoint.com>
2011-01-31 11:27:58 -0800

    Added rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DjTest::Application.config.secret_token = 'f3ef94ffb9e35b38118816c776935c5c27e63c1d548e891600afb53bca32767c4b3cfe3066a1a5e6628d997f48d7e6b8fa132776af7c04f1cd1651e32f4a1b24'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DjTest::Application.config.session_store :cookie_store, :key => '_DjTest_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DjTest::Application.config.session_store :active_record_store
